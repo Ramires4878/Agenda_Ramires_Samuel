1a50149ff6cc1db2798c7e675e932703b8bd5726
RamiresOliveira <ramiresoliveira.10@hotamil.com>
2021-04-16 01:42:11 -0300

    Primeiro Push

---
+++ AgendaGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AgendaGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
